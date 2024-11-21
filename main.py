3b6cda697cf0401363ba0b0c8dee9b2d3e2bb07a
Alexsander-Pavlov <pavlov.pa147@gmail.com>
2024-11-21 14:26:43 +0600

    export v2

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+
 from config.models.base import Base
-from config.models.db_helper import db_helper
-from api_v1 import router
+from config.database import db_connection
+from api_v1 import register_routers
+from app_includes import (
+    register_errors,
+    register_middlewares,
+    register_prometheus,
+)
+
+
+def start_app() -> FastAPI:
+    """
+    Создание приложения со всеми настройками
+    """
+    app = FastAPI(lifespan=lifespan)
+    register_routers(app=app)
+    register_errors(app=app)
+    register_middlewares(app=app)
+    register_prometheus(app=app)
+    return app
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
+    async with db_connection.engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
         yield
-    await db_helper.dispose()
+    await db_connection.dispose()
 
 
-app = FastAPI(lifespan=lifespan)
-app.include_router(router)
+app = start_app()
